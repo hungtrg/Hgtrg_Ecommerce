dc22012eba0aab118129019bdb546db75247b83c
Truong Hiep Hung <hungjt46@gmail.com>
2023-06-28 01:57:59 +0700

    feat: setup pattern
    - combining uow + generic repo

---
+++ Hgtrg.Ecommerce/Hgtrg.Ecommerce.DataLayer/DataAccess/IGenericRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Hgtrg.Ecommerce.DataLayer.DataAccess
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+    }
+
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext _dbContext;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public Repository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<TEntity>();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+#pragma warning disable CS8603 // Possible null reference return.
+            return await _dbSet.FindAsync(id);
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+
+        public void Add(TEntity entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
---
+++ Hgtrg.Ecommerce/Hgtrg.Ecommerce.DataLayer/DataAccess/IUnitOfWork.cs
@@ -0,0 +1,65 @@
+﻿using Hgtrg.Ecommerce.DataLayer.Models;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Hgtrg.Ecommerce.DataLayer.DataAccess
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+        Task<int> SaveChangesAsync();
+    }
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly HgtrgEcommerceContext _dbContext;
+        private IDbContextTransaction _transaction;
+        private readonly Dictionary<Type, object> _repositories;
+
+        public UnitOfWork(HgtrgEcommerceContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+            _repositories = new Dictionary<Type, object>();
+        }
+
+        public IRepository<TEntity> GetRepository<TEntity>() where TEntity : class
+        {
+            if (_repositories.TryGetValue(typeof(TEntity), out var repository))
+            {
+                return (IRepository<TEntity>)repository;
+            }
+
+            var newRepository = new Repository<TEntity>(_dbContext);
+            _repositories.Add(typeof(TEntity), newRepository);
+
+            return newRepository;
+        }
+
+        public async Task BeginTransactionAsync()
+        {
+            _transaction = await _dbContext.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            await _transaction.CommitAsync();
+        }
+
+        public async Task RollbackTransactionAsync()
+        {
+            await _transaction.RollbackAsync();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+    }
+}
